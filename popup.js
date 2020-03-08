6dc1dcb4daee452c065b9c68b9e247f49cbe7259
Eloi Pardo <eloipr94@gmail.com>
2020-03-08 20:38:08 +0100

    first commit

---
+++ popup.js
@@ -0,0 +1,17 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+"use strict";
+
+let hidePopupButton = document.getElementById("hidePopup");
+
+hidePopupButton.onclick = function(element) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.executeScript(tabs[0].id, { code: 'document.body.style.overflow = "";' });
+        chrome.tabs.executeScript(tabs[0].id, {
+            code: 'document.getElementsByClassName("fc-ab-root")[0].style.display = "none";'
+        });
+        window.close();
+    });
+};
